129aa39123af9834f9a0dffdbb38b66c252fca0e
Rudin Engineer <rudinengineer@gmail.com>
2024-10-24 19:31:21 +0700

    migration to nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,86 @@
-import Image from "next/image";
+"use client"
+import React from 'react'
+import { sendRequestTMDB } from '../lib/tmdb'
+import Movie from '../components/Movie'
+import MovieSkeleton from '../components/skeleton/MovieSkeleton'
+import { MovieType } from '../types/movie';
+import { useSearchParams } from 'next/navigation'
+import Slider from '~/components/ui/Slider'
+import Pagination from '~/components/ui/Pagination';
 
-export default function Home() {
+type Props = {}
+
+export default function Home({}: Props) {
+  const [response, setResponse] = React.useState<any>([])
+  const [movies, setMovies] = React.useState<Array<MovieType>>([])
+  const [isLoading, setLoading] = React.useState<boolean>(true)
+  const [paginateCount, setPaginateCount] = React.useState<Array<any>>([])
+  const params = useSearchParams()
+  const page: number = params.has('page') ? Number(params.get('page')) : 1
+
+  React.useEffect(() => {
+    setLoading(true)
+    async function fetchMovies() {
+      const response = await sendRequestTMDB('/trending/all/week?page=' + page)
+      const data = await response?.data
+      if ( data ) {
+        setResponse(data)
+        setMovies(data?.results)
+        let result = []
+        for (let index = 0; index < (Number(data?.total_pages) > 9 ? 9 : Number(data?.total_pages)); index++) {
+          if ( page > (Number(data?.total_pages) - page) ) {
+            result.push(page - index)
+          } else {
+            result.push(page + index)
+          }
+        }
+        setPaginateCount(result)
+        setLoading(false)
+      }
+    }
+
+    fetchMovies()
+  }, [page])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className="px-3 p-2 rounded-md overflow-hidden">
+        <Slider movies={movies} />
+      </div>
+      <div className="p-3">
+        <div>
+          <h1 className='text-2xl font-bold'>Paling Populer✨</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="mt-4 grid grid-cols-3 ss:grid-cols-4 sm:grid-cols-5 lg:grid-cols-6 gap-3">
+          {
+            isLoading ? (
+                [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20].map(({}, index: number) => (
+                    <MovieSkeleton key={index} />
+                ))
+            ) : (
+                movies.length > 0 && movies.map((value: any, index: number) => (
+                    <div className='mb-2' key={index}>
+                        <Movie movie={value} />
+                    </div>
+                ))
+            )
+          }
+        </div>
+
+        {
+          (!isLoading && !movies.length) && (
+              <div className="mt-2 w-full flex justify-center">
+                  <h1 className="text-center font-semibold">Data tidak ditemukan.</h1>
+              </div>
+          )
+        }
+
+        {
+          movies.length > 0 && (
+            <Pagination pagination={paginateCount} page={page} total_pages={response?.total_pages} />
+          )
+        }
+      </div>
     </div>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,27 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  mode: 'jit',
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+        default: '#434343',
+        primary: '#00a8e8',
+        dark: '#000000',
+        darkSecondary: '#1c1c1c',
+        // darkSecondary: '#060606',
+        light: '#fff',
+        secondary: '#e9e9e9',
+        icon: '#5f5f5f',
+      }
+    },
+    screens: {
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
     },
   },
   plugins: [],
